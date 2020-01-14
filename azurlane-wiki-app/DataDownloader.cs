85f3c46a33ad9ea1f18a5fd026a4a52aa6e6c55c
Dzmitry Mukhliada <n0menillislegio@yandex.by>
2020-01-14 20:12:58 +0300

    Created Project

---
+++ azurlane-wiki-app/DataDownloader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azurlane_wiki_app
+{
+    class DataDownloader
+    {
+        private string WikiAPIEndpoint = "https://azurlane.koumakan.jp/w/api.php";
+
+        public DataDownloader()
+        {
+            
+        }
+    }
+}
