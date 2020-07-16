66730a57ee92bdf2e199d02ff574724bab0c7681
Dzmitry Mukhliada <n0menillislegio@yandex.by>
2020-07-16 15:49:13 +0300

    Save frequently used icons in memory

--- azurlane-wiki-app/ImagePathConverter.cs
+++ azurlane-wiki-app/ImagePathConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
@@ -7,39 +8,75 @@ namespace azurlane_wiki_app
 {
     class ImagePathConverter : IValueConverter
     {
-        private const string ImagePlaceholder = "/Resources/Placeholder.png";
+        private static readonly BitmapImage ImagePlaceholder = 
+            new BitmapImage(new Uri(@"pack://application:,,,/Resources/Placeholder.png"));
+
+        private static Dictionary<string, BitmapImage> icons = new Dictionary<string, BitmapImage>();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string path = value?.ToString();
-            
 
             if (path == null)
             {
                 return null;
             }
 
-            ImageChecker imageChecker = new ImageChecker(path);
+            BitmapImage outputImage;
 
-            if (!imageChecker.IsComplete)
+            if (path.Contains("./Images/Icons/"))
             {
-                return new BitmapImage(new Uri(@"pack://application:,,," + ImagePlaceholder));
+                outputImage = GetIcon(path);
+            }
+            else
+            {
+                if (!(new ImageChecker(path).IsComplete))
+                {
+                    outputImage = ImagePlaceholder;
+                }
+                else
+                {
+                    try
+                    {
+                        outputImage = new BitmapImage(new Uri(@"pack://siteoforigin:,,,"
+                                                              + path.Remove(0, 1)));
+                    }
+                    catch
+                    {
+                        outputImage = null;
+                    }
+                }
             }
 
-            try
-            {
-                return new BitmapImage(new Uri(@"pack://siteoforigin:,,,"
-                                                            + path.Remove(0, 1)));
-            }
-            catch
-            {
-                return null;
-            }
+            return outputImage;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        private BitmapImage GetIcon(string iconPath)
+        {
+            BitmapImage bitmapImage;
+
+            if (!icons.ContainsKey(iconPath))
+            {
+                if (!(new ImageChecker(iconPath).IsComplete))
+                {
+                    return ImagePlaceholder;
+                }
+
+                bitmapImage = new BitmapImage(new Uri(@"pack://siteoforigin:,,,"
+                                                      + iconPath.Remove(0, 1)));
+                icons.Add(iconPath, bitmapImage);
+            }
+            else
+            {
+                bitmapImage = icons[iconPath];
+            }
+
+            return bitmapImage;
         }
     }
 }
