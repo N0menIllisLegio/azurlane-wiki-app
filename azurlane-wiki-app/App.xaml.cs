7d57b1cdfe4669b0a3b1fa1df4aa6f1d211d0c82
Dzmitry Mukhliada <n0menillislegio@yandex.by>
2020-08-17 00:58:19 +0300

    Fixes: Retrofit toggler appearance; Chibi shipgirl image margin; shipgirl's card name size.

--- azurlane-wiki-app/App.xaml.cs
+++ azurlane-wiki-app/App.xaml.cs
@@ -1,7 +1,5 @@
-﻿using azurlane_wiki_app.Data.Downloaders;
-using ShowMeTheXAML;
+﻿using ShowMeTheXAML;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace azurlane_wiki_app
